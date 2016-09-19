62898e4cffcc001697498f835e6e917dda11585a
akash <aakashsingh@gmail.com>
2016-09-19 19:09:06 +0200

    Updated code for TopTen

--- lab1/topten/src/sics/TopTen.java
+++ lab1/topten/src/sics/TopTen.java
@@ -15,54 +15,76 @@ import org.apache.hadoop.util.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class TopTen {
 
     public static class TopTenMapper extends Mapper<Object, Text, NullWritable, Text> {
-	    // Stores a map of user reputation to the record
         
+        // Stores a map of user reputation to the record
         private TreeMap<Integer,Text> repToRecordMap = new TreeMap<Integer,Text>();
-	    
+        
+        //log handler for logging
+        private static final Log log = LogFactory.getLog(TopTenMapper.class);
+    
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            Map<String, String> parsed = transformXmlToMap(value.toString());
-	        String userId = parsed.get("AccountId");
-            String reputation = parsed.get("Reputation");
             
-            // Add this record to our map with the reputation as the key
-            if(userId == null || reputation==null) return;
+            //uncomment this to see that value is an single xml record. 
+            //log.info("======>"+value);
+    
+            Map<String, String> parsed = transformXmlToMap(value.toString());
+            
+            //retrieve relevant information from the map
+            String accountId = parsed.get("Id");
+            String reputation = parsed.get("Reputation");
+            if(accountId == null || "".equals(accountId.trim()) || reputation==null || "".equals(reputation.trim())) return;
+
+            // Add this record to TreeMap using reputation as the key
             repToRecordMap.put(Integer.parseInt(reputation),new Text(value));
 
-            // If we have more than ten records, remove the one with the lowest reputation.
+            // if the size of tree map becomes more than 10 records, delete the minimum key from map
             if (repToRecordMap.size() > 10) {
                 repToRecordMap.remove(repToRecordMap.firstKey());
             }
-	    }
+        }
 
         protected void cleanup(Context context) throws IOException, InterruptedException {
-        // Output our ten records to the reducers with a null key
+            //output the key.value pair for reducers. Key is null.
             for (Text t : repToRecordMap.values()) {
                 context.write(NullWritable.get(), t);
             }
         }   
-	}
-    
+    }
+
 
     public static class TopTenReducer extends Reducer<NullWritable, Text, NullWritable, Text> {
-        // Stores a map of user reputation to the record
-        // Overloads the comparator to order the reputations in descending order
+        
         private TreeMap<Integer, Text> repToRecordMap = new TreeMap<Integer, Text>();
+        public static final Log log = LogFactory.getLog(TopTenReducer.class);
 
         public void reduce(NullWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+	    int accountId;
+	    int reputation;
             for (Text value : values) {
-                Map<String, String> parsed = transformXmlToMap(value.toString());
-                String reputation = parsed.get("Reputation");
-                repToRecordMap.put(Integer.parseInt(parsed.get("Reputation")),new Text(parsed.get("DisplayName")+"::"+parsed.get("Reputation")));
 
-                // If we have more than ten records, remove the one with the lowest reputation
+                Map<String, String> parsed = transformXmlToMap(value.toString());
+                String displayName = parsed.get("DisplayName");
+		try{
+			accountId = Integer.parseInt(parsed.get("Id"));
+		        reputation = Integer.parseInt(parsed.get("Reputation"));
+		}
+		catch(NumberFormatException nfe){
+			continue;
+		}	
+                repToRecordMap.put(Integer.parseInt(parsed.get("Reputation")),new Text(accountId+"::"+displayName+"::"+reputation));
+
+                //if the size of tree map becomes more than 10 records, delete the minimum key from map
                 if (repToRecordMap.size() > 10) {
                     repToRecordMap.remove(repToRecordMap.firstKey());
                 }  
             }
-
+	    log.info("=====>Final Map Size:"+repToRecordMap.size());
             for (Text t : repToRecordMap.descendingMap().values()) {
                 // Output our ten records to the file system with a null key
                 context.write(NullWritable.get(), t);
@@ -70,11 +92,15 @@ public class TopTen {
         }
     }   
 
+    /*
+    * Utility function to trasnform xml string into map.
+    * Code taken from https://github.com/adamjshook/mapreducepatterns/blob/master/MRDP/src/main/java/mrdp/utils/MRDPUtils.java 
+    */
     public static Map<String, String> transformXmlToMap(String xml) {
         Map<String, String> map = new HashMap<String, String>();
         try {
             String[] tokens = xml.trim().substring(5, xml.trim().length() - 3)
-                    .split("\"");
+                .split("\"");
 
             for (int i = 0; i < tokens.length - 1; i += 2) {
                 String key = tokens[i].trim();
@@ -89,46 +115,20 @@ public class TopTen {
         return map;
     }
 
-    /*public static void main(String[] args) throws Exception {
-	JobConf conf = new JobConf(TopTen.class);
-	conf.setJobName("topten");
-
-	conf.setOutputKeyClass(NullWritable.class);
-	conf.setOutputValueClass(Text.class);
-
-	conf.setMapperClass(TopTenMapper.class);
-	conf.setCombinerClass(TopTenReducer.class);
-	conf.setReducerClass(TopTenReducer.class);
-
-	conf.setInputFormat(TextInputFormat.class);
-	conf.setOutputFormat(TextOutputFormat.class);
-
-    conf.setNumReduceTasks(1);
-    
-	FileInputFormat.setInputPaths(conf, new Path(args[0]));
-	FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-
-	JobClient.runJob(conf);
-    }*/
-
-        public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        String[] otherArgs = new GenericOptionsParser(conf, args)
-                .getRemainingArgs();
-        if (otherArgs.length != 2) {
-            System.err.println("Usage: TopTen <in> <out>");
-            System.exit(2);
-        }
-
-        Job job = new Job(conf, "Top Ten Users by Reputation");
+        
+	Job job = Job.getInstance(conf, "TopTen");
         job.setJarByClass(TopTen.class);
+
         job.setMapperClass(TopTenMapper.class);
         job.setReducerClass(TopTenReducer.class);
         job.setNumReduceTasks(1);
         job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
